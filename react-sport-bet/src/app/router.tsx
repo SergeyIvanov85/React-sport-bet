bcc2b53928947269fe43533d664ffc06f2c1066e
Sergey Ivanov <koldyn85@yandex.ru>
2023-05-31 22:04:09 +0300

    add: router, events, data; create: homepage

--- react-sport-bet/src/app/router.tsx
+++ react-sport-bet/src/app/router.tsx
@@ -1,1 +1,10 @@
-import { Navigate, createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
+
+import HomePage from '@/pages/HomePage';
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomePage />
+    }
+]);
