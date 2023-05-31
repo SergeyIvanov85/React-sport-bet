c1d75ac5127224c1fc991c5663228852ef57fb40
Sergey Ivanov <koldyn85@yandex.ru>
2023-05-31 23:19:21 +0300

    add: styles

--- react-sport-bet/src/pages/HomePage.tsx
+++ react-sport-bet/src/pages/HomePage.tsx
@@ -1,14 +1,12 @@
-import EventList from '@/components/EventList/EventList';
-
+import EventBlock from '@/components/EventBlock/EventBlock';
+import { MainWrapper } from './HomePage.styles';
 import { events } from '@/data/Events';
 
 const HomePage = () => (
-    <>
-        <h2>Текущие матчи</h2>
-        <EventList events={events.filter((event) => event.type === 'live')} />
-        <h2>Предстоящие матчи</h2>
-        <EventList events={events.filter((event) => event.type === 'upcoming')} />
-    </>
+    <MainWrapper>
+        <EventBlock heading='Текущие события' events={events.filter(event => event.type === 'live')} />
+        <EventBlock heading='Предстоящие события' events={events.filter(event => event.type === 'upcoming')} />
+    </MainWrapper>
 );
 
 export default HomePage;
